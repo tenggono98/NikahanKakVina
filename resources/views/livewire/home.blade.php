769e4bae20838e9459e7d6cb96cfee950af271a4
tenggono98 <45934316+tenggono98@users.noreply.github.com>
2023-06-15 02:53:36 +0700

    upp

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -30,16 +30,16 @@
     <section id="info" class="" >
         <div class="flex  justify-center items-center flex-col">
             <div class="w-full h-[100%]  pt-10 pb-10 px-3 md:pt-0  md:px-0 md:pb-0  bg-fixed bg-center" style="background-image: url('{{asset('img/BACKGROUND.jpg')}}')">
-                <img src="{{asset('img/Asset 2.svg')}}" class=" bg-contain  w-[98%]  " alt="">
+                <img src="{{asset('img/Asset 2.svg')}}" class=" bg-contain  w-[98%] md:mt-5  " alt="">
                 <div class="w-full h-full flex flex-col  justify-start items-center">
 
 
 
-                        <div class="grid grid-cols-4 md:grid-cols-3  h-full w-full  md:pt-12 md:px-20 gap-3">
+                        <div class="grid grid-cols-4 md:grid-cols-3   h-max w-full  md:pt-12 md:px-20 gap-3">
                             <div class="order-2 md:order-none col-span-2 md:col-span-1">
                                 <img src="{{ asset('img/laki-laki.png')}}" class="mx-auto w-auto h-52 md:w-auto md:h-2/3" alt="">
-                                <p class="uppercase text-[#582841] text-sm md:text-3xl text-center" style="font-family: geovano;">Arie Lasmana</p>
-                                <p class="text-[#582841] text-center text-xs md:text-xl " style="font-family: spinnaker;">Son of ( &#128327;  ) Mr. Alimin Effendy <br>&<br> Mrs. Lis Harjanti</p>
+                                <p class="uppercase text-[#582841] text-sm md:text-3xl text-center" style="font-family: geovano;">Arie Lesmana</p>
+                                <p class="text-[#582841] text-center text-xs md:text-xl " style="font-family: spinnaker;">Son of ( &#128327;  ) Mr. Alimin Effendy <br>&<br> Mrs. Lis Haryati</p>
                             </div>
                             <div class="mt-10 md:mt-32 order-1 col-span-4 md:col-span-1 md:order-none">
                                 <p class="text-[#582841] text-center text-md md:text-3xl " style="font-family: spinnaker;">The honor of your presence is requested.<br>
@@ -60,10 +60,217 @@
 
 
                 </div>
-                <img src="{{asset('img/Asset 3.svg')}}" class=" bg-contain w-[98%]  " alt="">
+                <img src="{{asset('img/Asset 3.svg')}}" class=" bg-contain w-[98%] md:mb-5 " alt="">
 
             </div>
         </div>
+    </section>
+
+
+    <section id="info">
+        <div class="w-full h-[100vh]  pt-10 pb-10 px-3 md:pt-0  md:px-0 md:pb-0  bg-[#ab212a]" >
+            <div class="grid grid-cols-12 px-24 py-24">
+
+
+                <div class=" col-span-5">
+                    {{-- Title --}}
+                        <p class="text-7xl font-bold text-white text-center ">SAVE THE DATE</p>
+                        <img src="{{asset('img/Asset 1.svg')}}" class="h-[5vh] md:h-[20vh] mx-auto  -mt-7 " alt="">
+                    {{-- Title --}}
+
+
+                    {{-- Clock --}}
+
+                    <div class="flex mt-28 justify-center ">
+                        <div class="">
+                            <div class="bg-white px-8 py-6 my-auto">
+                                <p class="text-3xl" id="clock_days"></p>
+                            </div>
+                            <p class="text-center text-white mt-4">DAY</p>
+                        </div>
+                        <div class="self-center px-8 pb-12 h-full">
+                            <p class="text-white text-5xl">:</p>
+                        </div>
+                        <div class="">
+                            <div class="bg-white px-8 py-6 my-auto">
+                                <p class="text-3xl" id="clock_hours"></p>
+                            </div>
+                            <p class="text-center text-white mt-4">HOUR</p>
+                        </div>
+                        <div class="self-center px-8 pb-12 h-full">
+                            <p class="text-white text-5xl">:</p>
+                        </div>
+                        <div class="">
+                            <div class="bg-white px-8 py-6 my-auto">
+                                <p class="text-3xl" id="clock_minutes"></p>
+                            </div>
+                            <p class="text-center text-white mt-4">MINUTE</p>
+                        </div>
+                        <div class="self-center px-8 pb-12 h-full">
+                            <p class="text-white text-5xl">:</p>
+                        </div>
+                        <div class="">
+                            <div class="bg-white px-8 py-6 my-auto">
+                                <p class="text-3xl" id="clock_second"></p>
+                            </div>
+                            <p class="text-center text-white mt-4">SECOND</p>
+                        </div>
+
+
+                    </div>
+
+                    {{-- Clock --}}
+
+                    {{-- Logic Clock --}}
+                    <script>
+
+
+                    function countdown(deadline) {
+
+
+
+                    // Get the current time in UTC.
+                    const now = new Date();
+
+                    // Convert the deadline to a Date object in UTC.
+                    const deadlineDate = new Date(deadline);
+
+                    // Calculate the difference between the current time and the deadline.
+                    const difference = deadlineDate - now;
+
+                    // Get the days, hours, minutes, and seconds from the difference.
+                    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+                    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+                    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+                    document.getElementById('clock_days').innerHTML= `${days}`;
+                        document.getElementById('clock_hours').innerHTML= `${hours}`;
+                        document.getElementById('clock_minutes').innerHTML= `${minutes}`;
+                        document.getElementById('clock_second').innerHTML= `${seconds}`;
+
+                    // Update the countdown every second.
+                    setInterval(() => {
+                    // Get the current time in UTC.
+                    const now = new Date();
+
+                    // Calculate the difference between the current time and the deadline.
+                   let difference = deadlineDate - now;
+
+                    // Get the days, hours, minutes, and seconds from the difference.
+                    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+                    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+                    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+                    document.getElementById('clock_days').innerHTML= `${days}`;
+                        document.getElementById('clock_hours').innerHTML= `${hours}`;
+                        document.getElementById('clock_minutes').innerHTML= `${minutes}`;
+                        document.getElementById('clock_second').innerHTML= `${seconds}`;
+
+
+
+                    }, 1000);
+                    }
+
+
+                    countdown("2023-09-15T11:00:00+07:00");
+
+                    </script>
+                    {{-- Logic Clock --}}
+
+                    {{-- Kata-kata Mutiara --}}
+                    <div class="">
+                        <p class="text-center text-white mt-10" style="font-family:spinnaker">And now these three remain: faith, hope, and love. But the <br> greatest of these is love.<br>
+                            1 Corinthians 13:13
+                        </p>
+                    </div>
+                    {{-- Kata-kata Mutiara --}}
+
+                </div>
+
+                <div class="col-span-3">
+
+                    <div class="">
+                        <img src="" alt="">
+                    </div>
+
+                    <div class="">
+                        <img src="" alt="">
+                    </div>
+
+
+                </div>
+
+
+                <div class="col-span-4">
+
+                    {{-- Section 1 --}}
+                    <div class="">
+                        <p>HOLY MATRIMONY</p>
+                        <div class="flex justify-between">
+                            <div class="">
+                                <p>SABTU, 29 SEPTEMBER 2023 </p>
+
+                            </div>
+                            <div class="mr-24">
+                                <p>11.00</p>
+                            </div>
+                        </div>
+                        <p>
+                            Gereja Katolik Paroki St. Vincentius A Paulo Jl. Ananas No.41, Pisang Candi, Kec. Sukun, <br> Kota Malang, Jawa Timur 65146
+                        </p>
+                        <button class="btn bg-white ">
+                            Map Location
+                        </button>
+                    </div>
+
+                    {{-- Section 1 --}}
+
+
+
+                    {{-- Seperator--}}
+                    <div class="">
+                        <div class="">
+                            <img src="{{asset('img/Asset 2.svg')}}" class="   w-full h-[10em] md:mt-5 invert " alt="">
+                        </div>
+
+
+
+                    </div>
+
+                    {{-- Seperator--}}
+
+
+                    {{-- Section 2 --}}
+
+                    <div class="">
+                        <p>WEDDING RECEPTION</p>
+                        <div class="flex justify-between">
+                            <div class="">
+                                <p>SABTU, 29 SEPTEMBER 2023 </p>
+
+                            </div>
+                            <div class="mr-24">
+                                <p>18.00</p>
+                            </div>
+                        </div>
+                        <p>
+                            Gereja Katolik Paroki St. Vincentius A Paulo Jl. Ananas No.41, Pisang Candi, Kec. Sukun, <br> Kota Malang, Jawa Timur 65146
+                        </p>
+                        <button class="btn bg-white ">
+                            Map Location
+                        </button>
+                    </div>
+
+
+                    {{-- Section 2--}}
+
+                </div>
+
+            </div>
+        </div>
+
     </section>
 
 </div>
