3e4282c43db52350a93793529a76e5a32150b81b
tenggono98 <tenggono98@gmail.com>
2023-09-14 01:08:22 +0700

    Dashboard Function Done

--- app/Http/Livewire/Admin/Home.php
+++ app/Http/Livewire/Admin/Home.php
@@ -2,14 +2,74 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\CommentTamu;
+use App\Models\MTamu;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class Home extends Component
 {
+
+    public $total_tamu, $total_tamu_datang , $total_jumlah_tamu , $total_tamu_tidak_datang, $total_tamu_belum_respond , $total_tamu_belum_buka_link;
+    public $id_del;
+    use LivewireAlert;
+
+
+    protected $listeners = [
+        'confirmedDeleted'
+    ];
+
     public function render()
     {
+
+        $this->total_tamu = MTamu::all()->count();
+        $this->total_tamu_datang = MTamu::where('hadir','Iya')->count();
+        $this->total_jumlah_tamu = MTamu::sum('jumlah_tamu');
+        $this->total_tamu_tidak_datang = MTamu::where('hadir','Tidak')->count();
+        $this->total_tamu_belum_respond = MTamu::where('hadir','FALSE')->count();
+        $this->total_tamu_belum_buka_link = MTamu::where('visit_website_status','FALSE')->count();
+
+
+        $comment_tamu = CommentTamu::orderBy('id','DESC')->get();
+
+
         $page_name = 'Beranda';
         $page_title = 'Beranda';
-        return view('livewire.admin.home')->layout('components.layout-admin-main',compact('page_name','page_title'));
+        return view('livewire.admin.home',compact('comment_tamu'))->layout('components.layout-admin-main',compact('page_name','page_title'));
     }
+
+
+    public function toggleComment($id)
+    {
+        $comment = CommentTamu::find($id);
+
+        if($comment->status == 'true')
+        $comment->status = 'false';
+        else
+        $comment->status = 'true';
+
+        $comment->save();
+
+        $this->alert('success', 'Comment Tamu Sudah diubah!');
+
+    }
+
+    public function deleteComment($id){
+        $this->id_del = $id;
+        $this->alert('question', 'Yakin mau di Hapus?', [
+            'showConfirmButton' => true,
+            'confirmButtonText' => 'Yes',
+            'onConfirmed' => 'confirmedDeleted'
+        ]);
+    }
+
+    public function confirmedDeleted()
+    {
+        CommentTamu::find($this->id_del)->delete();
+
+        $this->id_del = null;
+
+        $this->alert('success', 'Comment Tamu Sudah dihapus!');
+    }
+
 }
--- resources/views/livewire/admin/home.blade.php
+++ resources/views/livewire/admin/home.blade.php
@@ -12,37 +12,67 @@
             <div class="bg-base-100 p-3 rounded-lg">
                 <h1 class="font-semibold text-3xl">Total Tamu</h1>
                 <hr class="my-2">
-                <p class="font-bold text-5xl">40 Tamu</p>
+                <p class="font-bold text-5xl">{{ $total_tamu ?? '0' }} Tamu</p>
             </div>
 
             <div class="bg-base-100 p-3 rounded-lg">
                 <h1 class="font-semibold text-3xl">Total Tamu Yang Datang</h1>
                 <hr class="my-2">
-                <p class="font-bold text-5xl">40 Tamu</p>
+                <p class="font-bold text-5xl">{{ $total_tamu_datang ?? '0' }} Tamu</p>
+            </div>
+
+            <div class="bg-base-100 p-3 rounded-lg">
+                <h1 class="font-semibold text-3xl">Total Jumlah Tamu Yang Datang</h1>
+                <hr class="my-2">
+                <p class="font-bold text-5xl">{{ $total_jumlah_tamu ?? '0' }} Tamu</p>
             </div>
 
             <div class="bg-base-100 p-3 rounded-lg">
                 <h1 class="font-semibold text-3xl">Total Tamu Yang Tidak Datang</h1>
                 <hr class="my-2">
-                <p class="font-bold text-5xl">40 Tamu</p>
+                <p class="font-bold text-5xl">{{ $total_tamu_tidak_datang ?? '0' }} Tamu</p>
             </div>
             <div class="bg-base-100 p-3 rounded-lg">
                 <h1 class="font-semibold text-3xl">Total Tamu Yang Belum Respond</h1>
                 <hr class="my-2">
-                <p class="font-bold text-5xl">40 Tamu</p>
+                <p class="font-bold text-5xl">{{ $total_tamu_belum_respond ?? '0' }} Tamu</p>
+            </div>
+
+            <div class="bg-base-100 p-3 rounded-lg">
+                <h1 class="font-semibold text-3xl">Total Tamu Yang Belum Buka Link</h1>
+                <hr class="my-2">
+                <p class="font-bold text-5xl">{{ $total_tamu_belum_buka_link ?? '0' }} Tamu</p>
             </div>
 
 
         </div>
 
 
-        <div class="text-base bg-base-100 p-3 w-full md:w-fit rounded-lg">
-            <h1 class="font-semibold text-3xl">Comment Terakhir</h1>
+        <div class="text-base bg-base-100 p-3 w-full  rounded-lg">
+            <h1 class="font-semibold text-3xl">List Comment Tamu</h1>
             <hr class="my-2">
-            <p class="font-bold text-3xl">"Semoga Menjadi Pengantin Yang Mantap"</p>
-            <small>By. John Doe</small>
+
+            @foreach ($comment_tamu as $item)
+            <div class="py-2">
+                <p class="font-bold text-3xl">"{{ $item->comment_tamu }}"</p>
+                <small>By. {{ $item->nama_tamu }}</small>
+                <div class="py-2 ">
+                    {{--  --}}
+                    <div class="">
+                        <button class="btn {{ ($item->status == 'true')? 'btn-secondary' : 'btn-accent' }} " wire:click="toggleComment('{{ $item->id }}')">{{ ($item->status == 'true')? 'Hide' : 'Show' }}</button>
+                        <button class="btn btn-error" wire:click="deleteComment('{{ $item->id }}')">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            @endforeach
+
+
         </div>
 
     </div>
+
+
+
 
 </div>
